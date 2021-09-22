4af71293a97331a39ed110db99a5a1898d1e059a
btopro <btopro@outlook.com>
2021-09-22 11:26:56 -0400

    some tweaks

--- src/PsuBtn.js
+++ src/PsuBtn.js
@@ -6,7 +6,53 @@ export class PsuBtn extends LitElement {
       :host {
         display: block;
         padding: 25px;
-        color: var(--psu-btn-text-color, #000);
+        --psu-btn-text-color: orange;
+      }
+      :host([id="cool"]),
+      :host([invert]) {
+        background-color: blue;
+        color: black;
+        margin: 100px;
+      }
+      button:focus,
+      button:hover {
+        background-color: green;
+        color: yellow;
+      }
+      a:disabled {
+
+      }
+      a {
+        box-shadow: 0px 0px 0px 2px #9fb4f2;
+        background:linear-gradient(to bottom, #7892c2 5%, #476e9e 100%);
+        background-color:#7892c2;
+        border-radius:10px;
+        border:1px solid #4e6096;
+        display:inline-block;
+        cursor:pointer;
+        color:#ffffff;
+        font-family:Arial;
+        font-size:19px;
+        padding:12px 37px;
+        text-decoration:none;
+        text-shadow:0px 1px 0px #283966;
+      }
+      a {
+        color: var(--psu-btn-text-color);
+      }
+      a:hover {
+        background:linear-gradient(to bottom, #476e9e 5%, #7892c2 100%);
+        background-color:#476e9e;
+      }
+      a:active {
+        position:relative;
+        top:1px;
+      }
+      button {
+        color: var(--psu-btn-text-color);
+        background-color: transparent;
+    display: block;
+    border: none;
       }
     `;
   }
@@ -14,24 +60,19 @@ export class PsuBtn extends LitElement {
   static get properties() {
     return {
       title: { type: String },
-      counter: { type: Number },
+      link: { type: String },
     };
   }
 
   constructor() {
     super();
-    this.title = 'Hey there';
-    this.counter = 5;
-  }
-
-  __increment() {
-    this.counter += 1;
+    this.title = 'Penn State';
+    this.link = "https://psu.edu/";
   }
 
   render() {
     return html`
-      <h2>${this.title} Nr. ${this.counter}!</h2>
-      <button @click=${this.__increment}>increment</button>
+      <a href="${this.link}" tabindex="-1"><button>${this.title}<slot></slot></button></a>
     `;
   }
 }
